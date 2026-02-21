0474c3baae13acdc9fa9fa8e9469ac78576f227c
Trey <47907719+trwy7@users.noreply.github.com>
2026-02-20 19:05:21 -0700

    feat: Hardware monitor page, no function yet

---
+++ src/apps/hwmonitor/__init__.py
@@ -0,0 +1,13 @@
+import threading
+import psutil
+from flask import render_template
+from init import App
+
+app = App("System", [])
+
+@app.blueprint.route("/launch")
+def launch():
+    return render_template(f"{app.dirname}/pages/hw.html")
+
+def send_stats_thread():
+    pass
