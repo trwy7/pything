63078afad7e303ee533a92ef6860b6314f9a050e
Trey <47907719+trwy7@users.noreply.github.com>
2026-02-21 21:03:08 -0700

    feat: network section on hwmonitor

--- src/apps/hwmonitor/__init__.py
+++ src/apps/hwmonitor/__init__.py
@@ -9,11 +9,17 @@ app = App("System", [
     BooleanSetting("gib", "Values in GiB", "If on, shows data values in GiB instead of GB", False)
 ])
 
+onet = psutil.net_io_counters()
+net_lu = onet.bytes_sent
+net_ld = onet.bytes_recv
+del onet
+
 @app.blueprint.route("/launch")
 def launch():
     return render_template(f"{app.dirname}/pages/hw.html")
 
 def send_stats_thread():
+    global net_lu, net_ld
     while True:
         if not app.should_poll():
             time.sleep(1)
@@ -21,12 +27,21 @@ def send_stats_thread():
         app.logger.debug("Updating hardware stats")
         ram = psutil.virtual_memory()
         gib = app.settings['gib'].get_value()
+        onet = psutil.net_io_counters()
+        net_tu = onet.bytes_sent
+        net_td = onet.bytes_recv
         payload = {
             'rp': ram.percent,
             'ru': humanize.naturalsize(ram.used, binary=gib),
             'rt': humanize.naturalsize(ram.total, binary=gib),
-            'cpu': psutil.cpu_percent(interval=None)
+            'cpu': psutil.cpu_percent(interval=None),
+            'netdt': humanize.naturalsize(net_td, binary=gib),
+            'netut': humanize.naturalsize(net_tu, binary=gib),
+            'netdr': humanize.naturalsize(net_td - net_ld, binary=gib),
+            'netur': humanize.naturalsize(net_tu - net_lu, binary=gib)
         }
+        net_lu = net_tu
+        net_ld = net_td
         app.send("upd", payload)
         time.sleep(1)
 
