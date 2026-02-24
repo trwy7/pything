82465a4686dfa0c52fb8d13751eed80ae31e642e
Trey <47907719+trwy7@users.noreply.github.com>
2026-02-24 10:59:54 -0700

    feat: More Playback data accepted Does remove `Device`, but it wasn't that useful anyway

--- src/apps/music/__init__.py
+++ src/apps/music/__init__.py
@@ -1,4 +1,10 @@
 """Provider for all music related things, an example of an advanced app"""
 from init import App
+from .types import playback, Playback
 
 app = App("Music provider", [])
+
+def playback_listener(pb: Playback):
+    app.logger.debug("Got playback update")
+
+playback.listener = playback_listener
--- src/apps/music/types.py
+++ src/apps/music/types.py
@@ -1,4 +1,5 @@
 from typing import Literal
+from collections.abc import Callable # why cant this be in typing :(
 class Artist:
     def __init__(self, platform: str, id: str, name: str):
         self.platform = platform
@@ -11,25 +12,30 @@ class Album:
         self.title = title
         self.artists = artists
 class Song:
-    def __init__(self, platform: str, id: str, title: str, album: Album, artists: list[Artist]):
+    def __init__(self, platform: str, id: str, title: str, album: Album, artists: list[Artist], duration: int):
         self.platform = platform
         self.id = id
         self.title = title
         self.album = album
         self.artists = artists
-class Device:
-    def __init__(self, name: str, type: Literal["phone", "computer", "unknown"]):
-        self.name = name
-        self.type = type
+        self.duration = duration
 class Playback:
-    def __init__(self, song: Song | None=None, device: Device | None=None):
+    def __init__(self):
+        self.song: Song | None = None
+        self.paused: bool | None = None
+        self.position: int | None = None
+        self.listener: Callable | None = None
+    def update(self, song: Song, paused: bool | None, position: int | None):
         self.song = song
-        self.device = device
-    def update(self, song: Song, device: Device):
-        self.song = song
-        self.device = device
+        self.paused = paused
+        self.position = position
+        if isinstance(self.listener, Callable):
+            self.listener(self)
     def reset(self):
         self.song = None
-        self.device = None
+        self.paused = None
+        self.position = None
+        if isinstance(self.listener, Callable):
+            self.listener(self)
 
 playback = Playback()
