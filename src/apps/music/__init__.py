6b9312b3e949e7bf58ba1809a38c2081030f0360
Trey <47907719+trwy7@users.noreply.github.com>
2026-02-25 17:57:30 -0700

    feat: Show song progress on client screen

--- src/apps/music/__init__.py
+++ src/apps/music/__init__.py
@@ -1,5 +1,5 @@
 """Provider for all music related things, an example of an advanced app"""
-from init import App
+from init import App, socket
 from .types import playback, Playback
 
 app = App("Music provider", [])
@@ -7,3 +7,7 @@ app = App("Music provider", [])
 @playback.on_update
 def playback_listener():
     app.logger.debug("Got playback update: %s", playback.song.title if playback.song else "None")
+    socket.emit("music", [
+        ((playback.position / playback.song.duration) * 100) if playback.song and playback.position else None,
+        playback.playing
+    ])
