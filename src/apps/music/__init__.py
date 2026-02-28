770105bd65be960dc0e13b3b6b3b17b267a72f9b
Trey <47907719+trwy7@users.noreply.github.com>
2026-02-28 11:51:00 -0700

    feat: working music page

--- src/apps/music/__init__.py
+++ src/apps/music/__init__.py
@@ -1,8 +1,22 @@
 """Provider for all music related things, an example of an advanced app"""
+from flask import render_template
 from init import App, socket
 from .types import playback
 
-app = App("Music provider", [])
+app = App("Music", [])
+
+@app.on("get")
+@playback.on_update
+def send_song(_=None):
+    app.send(
+        "song",
+        {
+            "title": playback.song.title,
+            "album": playback.song.album.title,
+            "artists": ", ".join([a.name for a in playback.song.artists]),
+            "art": playback.song.album.art
+        } if playback.song else None
+    )
 
 @playback.on_update
 def playback_listener():
@@ -11,3 +25,7 @@ def playback_listener():
         ((playback.position / playback.song.duration) * 100) if playback.song and playback.position else None,
         playback.playing
     ])
+
+@app.blueprint.route("/launch")
+def launch():
+    return render_template(f"{app.dirname}/pages/playing.html")
