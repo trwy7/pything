645879fd1ae77c5fafb6714648aa75b110a7ace3
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-03-01 13:10:49 -0700

    fix: remove custom apps from builds

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@ pyinstaller --onefile --name pything-linux \
 	--add-data "src/apps:apps" \
 	--add-data "src/pages:pages" \
 	--add-data "src/ctroot:ctroot" \
-	--add-data "src/customapps:customapps" \
 	--add-data "src/static:static" src/init.py
