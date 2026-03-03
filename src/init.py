954825896bffd4d116cdc4c8f8fe51810631e2b7
Trey <47907719+trwy7@users.noreply.github.com>
2026-03-03 08:44:15 -0700

    fix: Show hint that the app is running

--- src/init.py
+++ src/init.py
@@ -592,4 +592,5 @@ if __name__ == "__main__":
             threading.Thread(target=inject_thread, daemon=True).start()
         threading.Thread(target=clock_thread, daemon=True).start()
     # Start the server
+    logger.info("Starting on 127.0.0.1:5192")
     socket.run(app, "127.0.0.1", 5192, allow_unsafe_werkzeug=True)
